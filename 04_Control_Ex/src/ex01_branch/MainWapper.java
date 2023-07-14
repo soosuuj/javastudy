fcb77e73d278f77646d6d40ae5c073ce3f2996e5
soosuuj <jangsj2104@naver.com>
2023-07-14 09:44:00 +0900

    2023/07/14

--- 04_Control_Ex/src/ex01_branch/MainWapper.java
+++ 04_Control_Ex/src/ex01_branch/MainWapper.java
@@ -128,14 +128,14 @@ System.out.println(order + " 은(는)" + price + "입니다.");
 
   public static void ex05() {
     
-  //월에 따른 계절
-    //3 ~ 5 : 봄
-    //6 ~ 8 : 여름
-    //9 ~ 11: 가을
-    //12 ~ 2: 겨울
+  //월에 따른 계절         월 % 12
+    //3 ~ 5 : 봄            3 ~ 5
+    //6 ~ 8 : 여름          6 ~ 8
+    //9 ~ 11: 가을          9 ~ 11
+    //12 ~ 2: 겨울          0 ~ 2
     int month = 7;
     String season;  // "봄", "여름", "가을", "겨울"
-    
+    /*  
     switch(month) {
     case 3:
     case 4:
@@ -157,8 +157,46 @@ System.out.println(order + " 은(는)" + price + "입니다.");
     }
 System.out.println(month + " 은(는)" + season + "입니다.");
   }
+}
+  */
   
+  if(month >= 3 && month <= 5) {
+    season = "봄";
+  } else if(month >= 6 && month <= 8) {
+    season = "여름";
+  } else if(month >= 9 && month <= 11) {
+    season = "가을";
+  } else {
+    season = "겨울";    
+  }
+  System.out.println(month + " 월은 " + season + "입니다.");
+}
 
+  public static void ex05_1() {
+    
+    //월에 따른 계절         월 % 12 (나머지 범위...)
+      //3 ~ 5 : 봄            3 ~ 5
+      //6 ~ 8 : 여름          6 ~ 8
+      //9 ~ 11: 가을          9 ~ 11
+      //12 ~ 2: 겨울          0 ~ 2
+      int month = 1;
+      int mod = month % 12;
+      String season;  // "봄", "여름", "가을", "겨울"
+
+    if(mod <= 2 ) {
+      season = "겨울";
+    } else if(mod <= 5) {
+      season = "봄";
+    } else if(mod <= 8) {
+      season = "여름";
+    } else {
+      season = "가을";    
+    }
+    System.out.println(month + " 월은 " + season + "입니다.");
+  }
+  
+  
+  
   public static void ex06() {   
   
   //월에 따른 분기
@@ -168,26 +206,76 @@ System.out.println(month + " 은(는)" + season + "입니다.");
   //10 ~ 12 : 4분기
   int m = 7;
 
-  if(m <= 1 && m <= 3) {
+  if( m<= 3) {
     System.out.println("1분기");
-  } else if (m <= 4 && m <= 6) {
+  } else if ( m <= 6) {
     System.out.println("2분기");
-  }else if (m <= 7 && m <= 9) {
+  }else if ( m <= 9) {
     System.out.println("3분기");
-  } else if (m <= 10 && m <= 12) {
+  } else if ( m <= 12) {
     System.out.println("4분기");
   }
   }
 
+  public static void ex06_1() {   
+    
+    //월에 따른 분기      분기 계산
+    //1 ~ 3 : 1분기        (month - 1)  /3 + 1 (0, 1, 2) 는 3으로 나눴을 때 몫이 없음  /0 + 1 = 1분기
+    //4 ~ 6 : 2분기        (month - 1)  /3 + 1 (3, 4, 5) 는 3으로 나눴을 때 몫이 1 
+    //7 ~ 9 : 3분기        (month - 1)  /3 + 1 (0, 1, 2) 는 3으로 나눴을 때 몫이 2 
+    //10 ~ 12 : 4분기
+    int m = 7;
+    System.out.println ((m - 1) / 3 + 1 + "분기");
+  }
+  
+  
+ 
   public static void ex07() {
     //10일 후 요일은?
-    int day = 13;  // 13일은 목요일
+    int day = 14;  // 14일은 금요일
     int nDay = 10;  // 10일
     String weekname;  // "월", "화", "수", "목", "금", "토", "일"
     
+   switch((day += nDay) % 7) {
+   case 0 : weekname = "금";  break;
+   case 1 : weekname = "토";  break;
+   case 2 : weekname = "일";  break;
+   case 3 : weekname = "월";  break;
+   case 4 : weekname = "화";  break;
+   case 5 : weekname = "수";  break;
+   default : weekname = "목";
+   }
+   System.out.println (day + "일은 " + weekname + "요일입니다.");
   }
-
   
+  
+ 
+ public static void ex08() {
+    // 대소문자 변환 (구글링으로 아스키코드 검색 후 참고)
+    char ch = 'A';  // 임의의 대문자 또는 소문자
+
+    if(ch >= 65 && ch <= 90) {  //대문자라는 뜻
+      ch += 32;
+    } else if (ch > 90 && ch <= 122) {
+      ch -=32;
+    }
+    System.out.println(ch);
+ }
+ 
+ 
+ public static void ex08_1() {
+    // 대소문자 변환 (구글링으로 아스키코드 검색 후 참고)
+    char ch = 'A';  // 임의의 대문자 또는 소문자
+
+    if(ch >= 'A' && ch <= 'Z') {  //대문자라는 뜻
+      ch += 32;
+    } else if (ch > 'a' && ch <= 'z') {
+      ch -=32;
+    }
+    System.out.println(ch);
+ }
+ 
+
   public static void main(String[] args) {
     ex01();
     ex02();
@@ -195,8 +283,12 @@ System.out.println(month + " 은(는)" + season + "입니다.");
     ex03_1();
     ex04();
     ex05();
+    ex05_1();
     ex06();
+    ex06_1();
     ex07();
+    ex08();
+    ex08_1();
   }
 
 }
